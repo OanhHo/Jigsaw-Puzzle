ffc86e810ffe362a46691657947956a2f5ccf152
OanhHo <43032816+OanhHo@users.noreply.github.com>
2019-12-24 00:50:47 +0700

    Show image demo
    Show image demo

---
+++ Puzzle/Js/showimage.js
@@ -0,0 +1,17 @@
+﻿
+function jigsaw(canvasID, imageID, rows,columns) {
+    var image1;
+    var canvas;
+    var ctx;
+
+    this.canvasID = canvasID;
+    this.imageID = imageID;
+    this.mySelf = this;
+
+    this.initDrawing = function () {
+        mySelf = this;
+        canvas = document.getElementById(canvasID);
+
+        ctx = canvas.getContext('2d');
+        image1 = document.getElementById(imageID);
+    };
